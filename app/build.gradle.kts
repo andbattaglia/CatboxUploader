c5316a72e35797c92830a195e6a7b185b347da71
Andrea Battaglia <battaglia.andry@gmail.com>
2024-12-02 21:28:05 +0100

    feat: update libs.version.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,13 +34,3 @@ android {
         jvmTarget = "11"
     }
 }
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
