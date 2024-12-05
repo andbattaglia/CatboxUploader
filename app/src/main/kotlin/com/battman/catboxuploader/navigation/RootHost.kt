3922b4e85e37a7122261c685b7828df927c4b144
Andrea Battaglia <battaglia.andry@gmail.com>
2024-12-05 09:41:54 +0100

    feat: implement request permission view

--- app/src/main/kotlin/com/battman/catboxuploader/navigation/RootHost.kt
+++ app/src/main/kotlin/com/battman/catboxuploader/navigation/RootHost.kt
@@ -1,25 +1,41 @@
 package com.battman.catboxuploader.navigation
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.battman.feature.select.country.ui.navigation.SELECT_COUNTRY_ROUTE
 import com.battman.feature.select.country.ui.navigation.selectCountryScreen
+import com.battman.feature.select.photo.ui.navigation.SELECT_PHOTOS_ROUTE
 import com.battman.feature.select.photo.ui.navigation.navigateToSelectPhotos
 import com.battman.feature.select.photo.ui.navigation.selectPhotosScreen
 
 @Composable
 fun RootHost() {
+    val context = LocalContext.current
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = SELECT_COUNTRY_ROUTE,
+        startDestination = SELECT_PHOTOS_ROUTE,
     ) {
         selectCountryScreen(
             onNavigateToGallery = { navController.navigateToSelectPhotos() },
         )
 
-        selectPhotosScreen()
+        selectPhotosScreen(
+            onNavigateToPermissionSettings = { context.openActivitySettings() },
+        )
     }
+}
+
+private fun Context.openActivitySettings() {
+    val intent = Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", packageName, null),
+    )
+    startActivity(intent)
 }
