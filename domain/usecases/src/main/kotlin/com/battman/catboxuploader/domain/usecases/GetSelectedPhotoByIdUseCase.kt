d079182245c15e1c174ea863d9b7de1658c4ff84
Andrea Battaglia <battaglia.andry@gmail.com>
2024-12-07 23:54:02 +0100

    feat: implement features to manage photos

--- domain/usecases/src/main/kotlin/com/battman/catboxuploader/domain/usecases/GetSelectedPhotoByIdUseCase.kt
+++ domain/usecases/src/main/kotlin/com/battman/catboxuploader/domain/usecases/GetSelectedPhotoByIdUseCase.kt
@@ -3,18 +3,18 @@ package com.battman.catboxuploader.domain.usecases
 import arrow.core.Either
 import com.battman.catboxuploader.domain.models.ErrorType
 import com.battman.catboxuploader.domain.models.Photo
-import com.battman.catboxuploader.domain.usecases.DeleteSelectedPhotoUseCase.Params
 import com.battman.catboxuploader.photos.api.repositories.IPhotoRepository
 import com.battman.core.usecase.api.IUseCase
 import javax.inject.Inject
 
-class DeleteSelectedPhotoUseCase @Inject constructor(
+class GetSelectedPhotoByIdUseCase @Inject constructor(
     private val photoRepository: IPhotoRepository,
 )
-    : IUseCase<Params, Either<ErrorType, List<Photo>>>() {
+    : IUseCase<GetSelectedPhotoByIdUseCase.Params, Either<ErrorType, Photo?>>() {
 
-    data class Params(val photoID: Long)
+    data class Params(val photoId: Long)
 
     override suspend fun execute(params: Params) =
-        photoRepository.deleteCachedPhoto(params.photoID)
+        photoRepository.getCachedPhotos()
+            .map { photos -> photos.firstOrNull { it.id == params.photoId } }
 }
