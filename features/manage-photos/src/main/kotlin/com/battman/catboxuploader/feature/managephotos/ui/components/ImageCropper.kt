925263f6ea26eaff83198d2720c0fee4434d0af6
Andrea Battaglia <battaglia.andry@gmail.com>
2024-12-07 23:53:19 +0100

    feat: add image cropper view

---
+++ features/manage-photos/src/main/kotlin/com/battman/catboxuploader/feature/managephotos/ui/components/ImageCropper.kt
@@ -0,0 +1,33 @@
+package com.battman.catboxuploader.feature.managephotos.ui.components
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
+
+@Composable
+fun ImageCropper(
+    uri: Uri,
+    onCropSuccess: (Uri) -> Unit,
+    onCropFailure: (Throwable) -> Unit,
+) {
+    val imageCropLauncher = rememberLauncherForActivityResult(
+        contract = CropImageContract(),
+        onResult = { result ->
+            if (result.isSuccessful) {
+                onCropSuccess(result.uriContent!!)
+            } else {
+                val exception = result.error
+                onCropFailure(exception!!)
+            }
+        },
+    )
+
+    LaunchedEffect(Unit) {
+        val cropOptions = CropImageContractOptions(uri, CropImageOptions())
+        imageCropLauncher.launch(cropOptions)
+    }
+}
