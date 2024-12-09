3a1d873c07bec94afa7038a65c2b16c01d3c7c8c
Andrea Battaglia <battaglia.andry@gmail.com>
2024-12-09 23:56:53 +0100

    feat: implement progress request body

--- data/network-ext/src/main/kotlin/com/battman/data/network/utils/ProgressRequestBody.kt
+++ data/network-ext/src/main/kotlin/com/battman/data/network/utils/ProgressRequestBody.kt
@@ -1,5 +1,7 @@
 package com.battman.data.network.utils
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
@@ -9,15 +11,15 @@ import java.io.FileInputStream
 
 class ProgressRequestBody(
     private val file: File,
-    private val contentType: String,
-    private val callback: (progress: Int) -> Unit
+    private val contentType: String
 ) : RequestBody() {
 
-    override fun contentType(): MediaType? = contentType.toMediaTypeOrNull()
-
-    override fun contentLength(): Long = file.length()
-
-    override fun writeTo(sink: BufferedSink) {
+    /**
+     * Provides a `Flow` of progress updates during the file write operation.
+     *
+     * @return A `Flow` emitting progress as an integer percentage (0-100).
+     */
+    fun progressFlow(): Flow<Int> = flow {
         val inputStream = FileInputStream(file)
         val buffer = ByteArray(BUFFER_SIZE)
         var uploaded: Long = 0
@@ -25,25 +27,39 @@ class ProgressRequestBody(
 
         try {
             while (true) {
-
                 val read = inputStream.read(buffer)
                 if (read == -1) break
 
                 uploaded += read
-                sink.write(buffer, 0, read)
-
-                val progress = (((uploaded / fileSize.toDouble())) * 100).toInt()
-                callback(progress)
+                emit(((uploaded / fileSize.toDouble()) * 100).toInt())
             }
-
         } catch (e: Exception) {
-            e.printStackTrace()
+            throw e
+        } finally {
+            inputStream.close()
+        }
+    }
+
+    override fun contentType(): MediaType? = contentType.toMediaTypeOrNull()
+
+    override fun writeTo(sink: BufferedSink) {
+        val inputStream = FileInputStream(file)
+        val buffer = ByteArray(BUFFER_SIZE)
+
+        try {
+            while (true) {
+                val read = inputStream.read(buffer)
+                if (read == -1) break
+                sink.write(buffer, 0, read)
+            }
+        } catch (e: Exception) {
+            throw e
         } finally {
             inputStream.close()
         }
     }
 
     companion object {
-        const val BUFFER_SIZE = 1024
+        const val BUFFER_SIZE = 4096
     }
 }
