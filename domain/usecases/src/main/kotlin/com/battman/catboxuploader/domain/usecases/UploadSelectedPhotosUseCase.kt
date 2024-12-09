7b2e8583a0aa1b5c89a9a126b1d0be0354f2ad87
Andrea Battaglia <battaglia.andry@gmail.com>
2024-12-09 23:58:41 +0100

    feat: implement usecase to observe uploader

--- domain/usecases/src/main/kotlin/com/battman/catboxuploader/domain/usecases/UploadSelectedPhotosUseCase.kt
+++ domain/usecases/src/main/kotlin/com/battman/catboxuploader/domain/usecases/UploadSelectedPhotosUseCase.kt
@@ -1,20 +1,16 @@
 package com.battman.catboxuploader.domain.usecases
 
-import arrow.core.Either
-import arrow.core.right
-import com.battman.catboxuploader.domain.models.ErrorType
+import com.battman.catboxuploader.domain.models.UploadDigest
 import com.battman.catboxuploader.domain.usecases.UploadSelectedPhotosUseCase.Params
 import com.battman.catboxuploader.photos.api.repositories.IPhotoRepository
-import com.battman.core.usecase.api.IUseCase
+import com.battman.core.usecase.api.IFlowUseCase
 import javax.inject.Inject
 
 class UploadSelectedPhotosUseCase @Inject constructor(
     private val photoRepository: IPhotoRepository,
-)
-    : IUseCase<Params, Either<ErrorType, String>>() {
+) : IFlowUseCase<Params, UploadDigest>() {
 
     data object Params
 
-    override suspend fun execute(params: Params) = "".right()
-
+    override fun execute(params: Params) = photoRepository.upload()
 }
